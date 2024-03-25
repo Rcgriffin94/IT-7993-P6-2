49246de3dad6e6802b3b48a7ca1a965df9c9f855
Ryan Griffin <rcgriffin1994@gmail.com>
2024-03-24 20:56:05 -0400

    Added browse Records

---
+++ pages/3_Browse Records.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+
+st.set_page_config(
+        layout="wide",
+        page_title='KSU RSS Feed Parser • Analyze Records',
+        page_icon='📰'
+    )
+add_logo('ksu_logo_resized.png')
+
+st.title('Browse Data')
+st.write('Use https://arnaudmiribel.github.io/streamlit-extras/extras/dataframe_explorer/')
+
+st.image('under_construction.png')
