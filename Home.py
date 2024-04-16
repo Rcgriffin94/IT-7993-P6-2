631cd768cf13fc6f60ba9e68f851de881128f55e
Ryan Griffin <rcgriffin1994@gmail.com>
2024-04-15 20:55:02 -0400

    added disclainer

--- Home.py
+++ Home.py
@@ -11,6 +11,8 @@ add_logo('ksu_logo_resized.png')
 
 st.title('Welcome to our RSS Financial News Feed Parser!')
 
+st.write('Disclaimer: This tool is currently in beta and is not designed to be ran by multiple users at once due to API constraints.')
+
 st.write('This application will provide you with an AI sentiment analysis of recent financial news from across the internet. From here you can view the analysis using "Analyze Records" and filter by keyword and time frame. While a number of sources from across the internet are already in place, you have the ability to add additional sources to create a more thourough analysis - simply upload additional sources through the "Refresh Sources" option. Happy analysis!')
 
 st.subheader('By: Raissa Divinagracia, Ryan Griffin, Justin Mayercik, Joshua McMillion, and Jacob Sweat')
